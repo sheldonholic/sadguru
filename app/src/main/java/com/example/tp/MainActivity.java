1faa8960516184847777d32b29ee278436448023
stopwatch <waghamaleshamika2013@gmail.com>
2019-11-28 00:03:49 +0530

    sadguru

---
+++ app/src/main/java/com/example/tp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tp;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+    }
+    public void Menu(View v){
+        Intent intent=new Intent(getApplicationContext(),Nav.class);
+        startActivity(intent);
+    }
+}
